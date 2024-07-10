2d5dc0fe43068a86f89777c78ea0b841540b4cd5
Epp Haavasalu <epp.haavasalu@gmail.com>
2024-07-10 12:27:55 +0300

    read from csv file, display table

---
+++ graafik/js/components/App.js
@@ -0,0 +1,43 @@
+import FileInput from "./FileInput.js";
+
+export default {
+  components: { FileInput },
+
+  template: `
+      <h1>CSV File Upload</h1>
+      <FileInput @parsed-data="handleParsedData" />
+      <div v-if="csvData.length">
+        <h2>Parsed CSV Data</h2>
+        <table border="1">
+          <thead>
+            <tr>
+              <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(row, rowIndex) in csvData" :key="rowIndex">
+              <td v-for="(header, colIndex) in headers" :key="colIndex">{{ row[header] }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+  `,
+
+  data() {
+    return {
+      csvData: []
+    };
+  },
+  computed: {
+    headers() {
+      return this.csvData.length ? Object.keys(this.csvData[0]) : [];
+    }
+  },
+  
+  methods: {
+    handleParsedData(data) {
+      this.csvData = data;
+    }
+  }
+}
