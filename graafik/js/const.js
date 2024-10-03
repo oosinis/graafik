066c15d1ea40c0478f70ce686ff167e044da27b4
oosinis <sinisookasper@gmail.com>
2024-10-03 18:17:36 +0300

    cleanup

--- graafik/js/const.js
+++
@@ -1,9 +0,0 @@
-export const weekEnd = ['L', 'P'];
-export const sickLeave = 'H';
-export const vacation = 'P';
-export const requestOffDay = 'X';
-export const requestWorkDay = 'T';
-
-export const requestsList = [sickLeave, vacation, requestOffDay, requestWorkDay];
-
-
