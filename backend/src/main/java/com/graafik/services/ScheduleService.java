fbc648d4336d01cc89f07db40e7e66e82dd8bd66
oosinis <sinisookasper@gmail.com>
2025-02-13 18:43:36 +0200

    scheduleEndpoint

---
+++ backend/src/main/java/com/graafik/services/ScheduleService.java
@@ -0,0 +1,11 @@
+package com.graafik.services;
+import com.graafik.model.Schedule;
+import com.graafik.model.ScheduleRequest;
+
+
+public class ScheduleService {
+    public Schedule Generate(ScheduleRequest scheduleRequest) {
+        Schedule schedule = new Schedule();
+        return schedule;
+    }
+}
