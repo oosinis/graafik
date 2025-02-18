f8dbac2e8a9186c620f4e0346d6b3ea003332cb5
epp <epp.haavasalu@gmail.com>
2025-02-18 17:54:59 +0200

    required rest days

--- backend/src/main/java/com/graafik/schedule/RuleValidator.java
+++ backend/src/main/java/com/graafik/schedule/RuleValidator.java
@@ -29,16 +29,16 @@ public class RuleValidator {
 
             int date = (currentSchedule.getDaySchedules() == null) ? 0 : currentSchedule.getDaySchedules().size();
 
-            int countCont = checkContinuous(shiftAssignment, currentSchedule, currentDayShiftAssignments, date - 1);
+            int countCont = checkContinuousNewAssignment(shiftAssignment, currentSchedule, currentDayShiftAssignments, date - 1);
 
             // if too many continuous days of this shift
             if (countCont <= -1) {
                 return -1000;
             }
 
-            System.out.println();
-/* 
-            for (int i = daySchedulesSize - 1 - countCont; i >= 0; i--) {
+            date = date - 1 - countCont;
+
+            for (int i = date; i >= 0; i--) {
 
                 DaySchedule daySchedule = currentSchedule.getDaySchedules().get(i);
                 ShiftAssignment previousShiftAssignment = DaySchedule.containsWorker(daySchedule, worker);
@@ -66,12 +66,47 @@ public class RuleValidator {
                 }
 
             }
-                */
+                
 
         
 
         }
         return 0;
+    }
+
+    private static int checkContinuousNewAssignment(ShiftAssignment shiftAssignment, Schedule currentSchedule, DaySchedule currentDayShiftAssignments, int date) {
+        int countCont = 0;
+        List<Rule> rules = shiftAssignment.getShift().getRules();
+        WorkerDto worker = shiftAssignment.getWorker();
+        System.out.println("\n KONTROLL: date: " + date);
+        System.out.println("looking at shift: " + shiftAssignment.toString());
+
+        for (int i = date; i >= 0; i--) {
+
+            DaySchedule daySchedule = currentSchedule.getDaySchedules().get(i);
+            ShiftAssignment previousShiftAssignment = DaySchedule.containsWorker(daySchedule, worker);
+            if (previousShiftAssignment == null) return countCont;
+            System.out.println("previous shift assignment: " + previousShiftAssignment.toString());
+            System.out.println("on date: " + date + "\n");
+
+
+            if (previousShiftAssignment.getShift() == shiftAssignment.getShift()) {
+                countCont++;
+                System.out.println("cont coutn " + countCont);
+                List<Rule> standingRules = new ArrayList<>();
+                for (Rule rule : rules) {
+                    System.out.println("rule cont days: " + rule.getContinuousDays());
+                    if (rule.getContinuousDays() > countCont) standingRules.add(rule);
+                }
+
+                if (standingRules.isEmpty()) {
+                    System.out.println("\nempty rules !!!!\n");
+                    return -1;
+                }
+                else rules = standingRules;
+            } else return countCont;
+        }
+        return countCont;
     }
 
     private static int checkContinuous(ShiftAssignment shiftAssignment, Schedule currentSchedule, DaySchedule currentDayShiftAssignments, int date) {
@@ -96,7 +131,7 @@ public class RuleValidator {
                 List<Rule> standingRules = new ArrayList<>();
                 for (Rule rule : rules) {
                     System.out.println("rule cont days: " + rule.getContinuousDays());
-                    if (rule.getContinuousDays() > countCont) standingRules.add(rule);
+                    if (rule.getContinuousDays() >= countCont) standingRules.add(rule);
                 }
 
                 if (standingRules.isEmpty()) {
