9d6e03d833b9c8547f1484712e2f4a167d80d984
oosinis <sinisookasper@gmail.com>
2025-07-22 23:38:24 +0300

    v2 standard

--- web-app/middleware.ts
+++ web-app/middleware.ts
@@ -1,33 +1,18 @@
-import { NextResponse } from "next/server";
-import { auth0 } from "./lib/auth0";
 import type { NextRequest } from "next/server";
+import { auth0 } from "./lib/auth0";
 
 export async function middleware(request: NextRequest) {
-    const authRes = await auth0.middleware(request);
-
-    // authentication routes — let the middleware handle it
-    if (request.nextUrl.pathname.startsWith("/auth")) {
-        return authRes;
-    }
-
-    // public routes — no need to check for session
-    if (request.nextUrl.pathname === "/") {
-        return authRes;
-    }
-
-    const { origin } = request.nextUrl;
-    const session = await auth0.getSession(request);
-
-    // user does not have a session — redirect to login
-    if (!session) {
-        return NextResponse.redirect(`${origin}/auth/login`);
-    }
-
-    return authRes;
+  return await auth0.middleware(request);
 }
 
 export const config = {
-    matcher: [
-        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api).*)",
-    ],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
