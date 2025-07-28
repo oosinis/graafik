067d546f6896516d3db332bd9182b1ac219ee7b3
oosinis <sinisookasper@gmail.com>
2025-07-28 22:40:01 +0300

    login page change

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,13 +1,22 @@
-// web-app/app/page.tsx
 "use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export default function RootRedirect() {
   const router = useRouter();
+  const { user, isLoading } = useUser();
+
   useEffect(() => {
-    router.replace("/login");
-  }, [router]);
+    if (isLoading) return;
+
+    if (user) {
+      router.replace("/dashboard");
+    } else {
+      router.replace("/login");
+    }
+  }, [user, isLoading, router]);
+
   return null;
 }
