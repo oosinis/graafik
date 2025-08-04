cdd52b2e4d40e8711cf0b5d7ee83ef727ee8c6f5
MaLasn <lasnmargareth8@gmail.com>
2025-08-04 18:05:59 +0300

    Multi step planner deleted and its contents made into separate components

--- web-app/app/generator/page.tsx
+++ web-app/app/generator/page.tsx
@@ -1,11 +1,19 @@
 import { MonthsHoursStep } from "@/components/monthsHoursStep"
+import { ShiftDetailsStep } from "@/components/ShiftDetailsStep"
+import { RuleDetailsStep } from "@/components/RuleDetailsStep"
+import { AssignEmployeesStep } from "@/components/AssignEmployeesStep"
 import { PageHeader } from "@/components/page-header"
 
 export default function GeneratorRoute() {
+  /* TODO: fullTimeYear and month info needs to be saved here and communicated to the components that 
+  use them */
   return (
     <div className="max-w-6xl mx-auto">
       <PageHeader title="Graafiku Generaator" description="Loo uus töögraafik" />
       <MonthsHoursStep />
+      <ShiftDetailsStep />
+      <RuleDetailsStep />
+      <AssignEmployeesStep />
     </div>
   )
 }
