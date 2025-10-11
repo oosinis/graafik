1814e3c0b58703613c262d7f2d690395ef43ef80
MaLasn <lasnmargareth8@gmail.com>
2025-10-11 12:41:31 +0300

    Delete nupp asukoht shift details

--- web-app/app/generator/ShiftDetailsStep.tsx
+++ web-app/app/generator/ShiftDetailsStep.tsx
@@ -179,7 +179,7 @@ export function ShiftDetailsStep({
 
         {!!active && !adding && (
           <>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
               <div>
                 <label className="block text-sm font-medium mb-2">Shift Title</label>
                 <Input
@@ -188,7 +188,8 @@ export function ShiftDetailsStep({
                 />
               </div>
 
-              <div>
+              <div className='flex justify-between'>
+                <div>
                 <label className="block text-sm font-medium mb-2">Shift Length</label>
                 <div className="flex items-center gap-2">
                   <Input
@@ -221,7 +222,16 @@ export function ShiftDetailsStep({
                   />
                   <span>min</span>
                 </div>
+                </div>
+                <div className='flex items-center'>
+                  <Button
+                    variant="destructive"
+                    onClick={() => onDeleteShift?.(active.id)}>
+                    Delete shift
+                  </Button>
+                </div>
               </div>
+
             </div>
 
             <RuleDetailsStep
@@ -235,13 +245,7 @@ export function ShiftDetailsStep({
               onAddRule={rulesProps.onAddRule}         
               onDeleteRule={rulesProps.onDeleteRule}
             />
-            <div>
-                <Button
-                variant="destructive"
-                onClick={() => onDeleteShift?.(active.id)}>
-                  Delete shift
-                </Button>
-              </div>
+            
           </>
         )}
       </Card>
