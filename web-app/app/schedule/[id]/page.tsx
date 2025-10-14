f9a6feb4b9ac6daa45eab314f61711052f91e12c
oosinis <sinisookasper@gmail.com>
2025-10-14 21:11:11 +0300

    roleguard and dashboard

--- web-app/app/schedule/[id]/page.tsx
+++ web-app/app/schedule/[id]/page.tsx
@@ -1,4 +1,7 @@
 // app/schedule/[id]/page.tsx
+"use client"
+
+import { RoleGuard } from "@/components/RoleGuard";
 import ScheduleGridView from "@/components/schedule-grid-view";
 
 interface SchedulePageProps {
@@ -9,9 +12,11 @@ export default async function SchedulePage({ params }: SchedulePageProps) {
   const scheduleId = params.id;
 
   return (
-    <div className="max-w-full mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">Schedule overview</h1>
-      <ScheduleGridView scheduleId={scheduleId} />
-    </div>
+    <RoleGuard allowedRoles={['Admin', 'Manager']}>
+      <div className="max-w-full mx-auto p-6">
+        <h1 className="text-2xl font-bold mb-4">Schedule overview</h1>
+        <ScheduleGridView scheduleId={scheduleId} />
+      </div>
+    </RoleGuard>
   );
 }
