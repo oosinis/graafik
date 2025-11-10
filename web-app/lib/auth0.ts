3c8952ac7fcebf82f7080263c1bda525d967dd6d
MaLasn <lasnmargareth8@gmail.com>
2025-11-10 19:31:03 +0200

    Shifti nimekirja vaate esialgne stiil (ruledel vaja mudida veel, kuna andmed hetkel puuduvad - CRUD func ka)

--- web-app/lib/auth0.ts
+++ web-app/lib/auth0.ts
@@ -1,5 +1,3 @@
-// lib/auth0.ts
-
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
 type Auth0WithOptionalLogout = Auth0Client & {
@@ -13,7 +11,7 @@ export const auth0 = new Auth0Client({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  appBaseUrl: process.env.APP_BASE_URL,
+  appBaseUrl: process.env.AUTH0_BASE_URL,
   secret: process.env.AUTH0_SECRET,
 
   authorizationParameters: {
